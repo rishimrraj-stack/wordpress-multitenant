423ddeb389d60b46881936cdb6a740f793f168b3
Rishi Raj <rishi.mr.raj@gmail.com>
2026-04-15 13:33:01 +0530

    2nd commit

--- wp-content/themes/tenant1-theme/functions.php
+++ wp-content/themes/tenant1-theme/functions.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * Tenant1 Theme Functions
+ *
+ * @package Tenant1Theme
+ */
+
+/**
+ * Display tenant1 message in footer.
+ *
+ * @return void
+ */
 function tenant1_msg() {
-    echo "<h2>Tenant1 Active</h2>";
+	echo '<h2>Tenant1 Active</h2>';
 }
-add_action('wp_footer', 'tenant1_msg');
+
+add_action( 'wp_footer', 'tenant1_msg' );
--- wp-content/themes/tenant3-theme/functions.php
+++ wp-content/themes/tenant3-theme/functions.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * Tenant3 Theme Functions
+ *
+ * @package Tenant3Theme
+ */
+
+/**
+ * Display tenant3 message in footer.
+ *
+ * @return void
+ */
 function tenant3_msg() {
-    echo "<h2>Tenant3 Active</h2>";
+	echo '<h2>Tenant3 Active</h2>';
 }
-add_action('wp_footer', 'tenant3_msg');
+
+add_action( 'wp_footer', 'tenant3_msg' );
